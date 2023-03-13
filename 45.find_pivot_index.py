855be85e73b880a20d615fc394ac0af5233864a3
venom <eyueldaniel0921@gmail.com>
2023-03-13 10:16:48 +0300

    :fire:

---
+++ 45.find_pivot_index.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/find-pivot-index
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        left_sum = 0
+
+        for i in range(len(nums)):
+            right_sum = total - nums[i] - left_sum
+            if left_sum == right_sum:
+                return i
+            left_sum += nums[i]
+        return -1
